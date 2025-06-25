56017bc1d1ab03488dff9b7c56e2fb325e94390b
jattu8602 <chaurasiyajatin68@gmail.com>
2025-06-25 20:46:25 +0530

    refactor: update application ID and package name
    - Change application ID from 'xyz.appmaker.cagvbl' to 'in.dakshedu.app' across all relevant files
    - Update TWA manifest and Gradle build configurations to reflect the new package name
    - Ensure consistency in resource files and output metadata for the new application ID

--- app/build/generated/source/buildConfig/release/xyz/appmaker/cagvbl/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/xyz/appmaker/cagvbl/BuildConfig.java
@@ -5,7 +5,7 @@ package xyz.appmaker.cagvbl;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "xyz.appmaker.cagvbl";
+  public static final String APPLICATION_ID = "in.dakshedu.app";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1";
